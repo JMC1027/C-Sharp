ab134a41e0d73bee3b3b816dc19c9df034be30be
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-11-26 13:21:27 -0500

    First C# Program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remeber, we're \"Accounting\" on you!");
+        Console.ReadLine();
+       
+        
+    }
+}
