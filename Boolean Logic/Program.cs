0549fdcb615c572874f8e4e00a7ebc3bb1a569f4
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-11-29 13:22:38 -0500

    resubmit

--- Boolean Logic/Program.cs
+++ Boolean Logic/Program.cs
@@ -19,27 +19,27 @@ namespace Boolean_Logic
             int age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Have you ever had a DUI? Answer True/False");
             string DUI = Console.ReadLine();
-            bool hasDUI = false;
+            bool hasDUI = false; //bool variable 
             
                   
             if (DUI == "True")
             {
                 Console.WriteLine("You do have a DUI");
-                hasDUI = true;
+                hasDUI = true; //returns true 
             } else if (DUI == "False")
             {
                 Console.WriteLine("You do not have a DUI");
-                hasDUI = false;
+                hasDUI = false; // returns false 
 
             } else
             {
-                Console.WriteLine("Please answer True or False");
+                Console.WriteLine("Please answer True or False");// returns message saying " Please answer true or false
             }
             Console.WriteLine(hasDUI);   
             Console.WriteLine("How many speeding tickets do you have?");
-            int numTickets = Convert.ToInt32(Console.ReadLine());
+            int numTickets = Convert.ToInt32(Console.ReadLine()); //ticket number variable converted to int, user input 
             Console.WriteLine("Does The applicant qualify to continue the application process?");
-            bool qualified = ((age > 15) && !hasDUI && (numTickets <= 3));
+            bool qualified = ((age > 15) && !hasDUI && (numTickets <= 3)); //bool expression compared to user input (Will return True or False)
             Console.WriteLine(qualified);
             
             
