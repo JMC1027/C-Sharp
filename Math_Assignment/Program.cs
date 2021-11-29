07701c35a51d51e033f64334f34a66a9dec06ecb
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-11-29 08:44:53 -0500

    resubmit

--- Math_Assignment/Program.cs
+++ Math_Assignment/Program.cs
@@ -9,48 +9,48 @@ namespace Math_Assignment
     {
         static void Main(string[] args)
         {
-            // Person 
-            int num1, num2, sum;
+             // Person 
+            int num1, num2, num3, sum;
             Console.WriteLine("Anonymous Income Comparison Program: ");
             Console.WriteLine("Person 1");
             Console.WriteLine("Hourly Rate");
             num1 = Convert.ToInt32(Console.ReadLine());           
             Console.WriteLine("Hours Wroked?");
             num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Weeks Wroked?");
+            num3 = Convert.ToInt32(Console.ReadLine());
+             
             
 
             //Person 2
-            int num3, num4, total;
+            int num4, num5, num6, total;
             Console.WriteLine("Person 2");
             Console.WriteLine("Hourly Rate");
-            num3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Hours Wroked?");
             num4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours Wroked?");
+            num5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Weeks Wroked?");
+            num6 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Annual Salary of Person 1 is: ");
-            sum = num1 * num2;
-            Console.Write(num1 * num2);
+            sum = num1 * num2 * num3;
+            Console.Write(num1 * num2 * num3);
             Console.ReadLine();
 
             Console.WriteLine("Annual Salary of Person 2 is: ");
-            total = num3 * num4;
-            Console.Write(num3 * num4);
+            total = num4 * num5 * num6;
+            Console.Write(num4 * num5 * num6);
             Console.ReadLine();
             
             Console.WriteLine("Does Person 1 make more money than Person 2?");
-            int person1 = num1 * num2;
-            int person2 = num3 * num4;
+            int person1 = num1 * num2 * num3;
+            int person2 = num4 * num5 * num6;
 
-            bool tf = person1 >= person2;
+            bool tf = person1 > person2;
             Console.Write(tf);
             Console.ReadLine();
             
 
-           
-            
-
-
-            
 
         }
     }
