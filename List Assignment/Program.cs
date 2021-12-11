a3584e5fcbc05d0f81a322c8ecfc3c58f4e6100b
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-12-11 18:40:53 -0500

    sub

--- List Assignment/Program.cs
+++ List Assignment/Program.cs
@@ -10,61 +10,61 @@ namespace List_Assignment
     {
         static void Main(string[] args)
         {
-            //string[] teams = { "Islanders", "Bruins", "Penguins", "Capitals" };
-            //Console.WriteLine("Input some text");
-            //string userInput = Console.ReadLine();
-            //for (int i = 0; i < teams.Length; i++)
-            //{
-            //    Console.WriteLine(teams[i] + userInput);
-            //}
-            //Console.ReadLine();
+            string[] teams = { "Islanders", "Bruins", "Penguins", "Capitals" };
+            Console.WriteLine("Input some text");
+            string userInput = Console.ReadLine();
+            for (int i = 0; i < teams.Length; i++)
+            {
+                Console.WriteLine(teams[i] + userInput);
+            }
+            Console.ReadLine();
 
-            //for (int i = 0; i < teams.Length; i++)
-            //{
-            //    Console.WriteLine(teams[i]);
-            //}
-            //Console.ReadLine();
-            //for (int i = 0; i < teams.Length; i++)
-            //    while (i <= teams.Length)
-            //    {
-            //        Console.WriteLine(teams[0]);
-            //        break;
-            //    } 
-            //Console.ReadLine();
+            for (int i = 0; i < teams.Length; i++)
+            {
+                Console.WriteLine(teams[i]);
+            }
+            Console.ReadLine();
+            for (int i = 0; i < teams.Length; i++)
+                while (i <= teams.Length)
+                {
+                    Console.WriteLine(teams[0]);
+                    break;
+                }
+            Console.ReadLine();
 
-            //string[] music = { "Country", "Rap", "Rock", "EDM" };
-            //for (int i = 0; i < music.Length;)
-            //{
-            //    if (i < music.Length)
-            //    {
-            //        Console.WriteLine(music[1]);
-            //    }
-            //    if (i <= music.Length)
-            //    {
-            //        Console.WriteLine(music[0]);
-            //        break;
-            //    }
-            //}
-            //Console.ReadLine();
+            string[] music = { "Country", "Rap", "Rock", "EDM" };
+            for (int i = 0; i < music.Length;)
+            {
+                if (i < music.Length)
+                {
+                    Console.WriteLine(music[1]);
+                }
+                if (i <= music.Length)
+                {
+                    Console.WriteLine(music[0]);
+                    break;
+                }
+            }
+            Console.ReadLine();
 
-            //List<string> newTeams = new List<string>(){ "Raiders", "Giants", "Bills", "Jets" };
-            //Console.WriteLine("Please choose from the following: Raiders, Giants, Bills, Jets");          
-            //string newUserInput = Console.ReadLine();
-            //for (int i = 0; i < newTeams.Count; i++)
-            //{
+            List<string> newTeams = new List<string>() { "Raiders", "Giants", "Bills", "Jets" };
+            Console.WriteLine("Please choose from the following: Raiders, Giants, Bills, Jets");
+            string newUserInput = Console.ReadLine();
+            for (int i = 0; i < newTeams.Count; i++)
+            {
 
-            //    if(newTeams[i] == newUserInput)
-            //    {
-            //        Console.WriteLine("You chose: " + newUserInput);
-            //        Console.WriteLine("This has an index of: " + i);
-            //        break;
-            //    }
-            //    if (!newTeams.Contains(newUserInput))
-            //    {
-            //        Console.WriteLine("Input is not valid");
-            //    }
-            //}
-            //Console.ReadLine();
+                if (newTeams[i] == newUserInput)
+                {
+                    Console.WriteLine("You chose: " + newUserInput);
+                    Console.WriteLine("This has an index of: " + i);
+                    break;
+                }
+                if (!newTeams.Contains(newUserInput))
+                {
+                    Console.WriteLine("Input is not valid");
+                }
+            }
+            Console.ReadLine();
 
             List<string> newMusic = new List<string>() { "Country", "Rap", "EDM", "Rap" };
             Console.WriteLine("Please choose from the following: Country, Rap, EDM");
@@ -74,15 +74,34 @@ namespace List_Assignment
                 if (newMusic[i] == userInputTwo)
                 {
                     Console.WriteLine("You chose: " + userInputTwo);
+                    Console.WriteLine("This has an index of: " + i);
+
                 }
-                if (!newMusic.Contains(userInputTwo)){
+                if (!newMusic.Contains(userInputTwo))
+                {
                     Console.WriteLine("Input is invalid");
                 }
             }
             Console.ReadLine();
-        }
-        
 
+            List<string> beer = new List<string>() { "Bud Light", "Corona", "Miller", "Miller" };
+            List<string> beerCopy = new List<string>();
+            foreach (string item in beer)
+            {
+                if (beerCopy.Contains(item))
+                {
+                    Console.WriteLine(item + " has appeared before");
+                }
+                else
+                {
+                    Console.WriteLine(item + " has not appeared");
+                }
+                beerCopy.Add(item);
+            }
+            Console.ReadLine();
+        }
 
     }
+
+        
 }
