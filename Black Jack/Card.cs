7de62d12eabce24559d6e622bb6a526e12331dee
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-12-12 12:31:32 -0500

    submit

---
+++ Black Jack/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black_Jack
+{
+    public class Card
+    {
+        internal string Face;
+        public Card()
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
+
+        public string Suit { get; set; }
+        public string Base { get; set; }
+    }
+}
