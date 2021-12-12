dfe974344c8e3d4cb82fc3de2297317ecdf492f9
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-12-12 16:14:08 -0500

    submit

---
+++ Calling Methods/Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    class Numbers
+    {
+        public int AddTwoNumbers(int num) //method
+        {
+            return 10 + num;
+        }
+        public int SquareTheNumber(int num)
+        {
+            return num * num;
+        }
+        public int DivideTheNumber(int num)
+        {
+            return num / 2; 
+        }
+    }
+}
---
+++ Calling Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers anum = new Numbers(); //calling method from program.cs
+            Console.WriteLine("What number do you want to add 10 to?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = anum.AddTwoNumbers(number);
+            Console.WriteLine("Here is the results of your math operation: " + result);
+            Console.ReadLine();
+
+            Console.WriteLine("What is a number you'd like to square?");
+            number = Convert.ToInt32(Console.ReadLine());
+            result = anum.SquareTheNumber(number);
+            Console.WriteLine("Here is the results of your math operation: " + result);
+            Console.ReadLine();
+
+            Console.WriteLine("What is a number you'd like to divide by 2?");
+            number = Convert.ToInt32(Console.ReadLine());
+            result = anum.DivideTheNumber(number);
+            Console.WriteLine("Here is the results of your math operation: " + result);
+            Console.ReadLine();
+
+        }
+    }
+}
