5ee25e317ea1bc040a046ffdfacc3bb46114029c
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-12-12 11:22:25 -0500

    submit

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> myNumbers = new List<int>() { 22, 35, 67, 89, 91, 95, 93 };
+                Console.WriteLine("Pick a number you would like to use to divide with");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < myNumbers.Count; i++)
+                {
+                    Console.WriteLine(myNumbers[i] / userInput); //divides myNumbers List by the user input 
+                }
+                Console.ReadLine();
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Please dont divide by zero");
+            }
+            Console.ReadLine();
+            Console.WriteLine("The program has just completed a try/catch block");
+            Console.ReadLine();
+            
+        }
+        
+    }
+}
