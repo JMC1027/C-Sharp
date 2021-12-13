136219c7bd50e771cce6a437f3c1e848558c22dc
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-12-13 11:09:18 -0500

    submit

--- Calling Methods 2/Integers.cs
+++ Calling Methods 2/Integers.cs
@@ -8,13 +8,14 @@ namespace Calling_Methods_2
 {
     public class Integers
     {
-        public int MathOperations(int number1)
+        public int MathOperations(int number1) //part 1
         {
             return 20 + number1;
         }
-        public int MathOperations(int number2, int number3)
+        public int MathOperations(int number2, int number3) //psrt 3
         {
             return number2 * number3;
         }
+     
     }
 }
--- Calling Methods 2/Program.cs
+++ Calling Methods 2/Program.cs
@@ -10,24 +10,43 @@ namespace Calling_Methods_2
     {
         static void Main(string[] args)
         {
-            Integers num = new Integers();
+            Integers num = new Integers(); //part 2
             Console.WriteLine("Please choose any number you'd like to add to 20");
             int number1 = Convert.ToInt32(Console.ReadLine());
             int result = num.MathOperations(number1);
             Console.WriteLine("The total is: " + result);
             Console.ReadLine();
 
-            Integers integers = new Integers(); //instantiate the class
-            integers.MathOperations(number1); //calling the method
+            Integers add = new Integers(); //instantiate the class
+            add.MathOperations(number1); //calling the method
             Console.WriteLine("The user input is: " + number1); //passes and displays the input int 
             Console.ReadLine();
 
+
             Console.WriteLine("Please enter a whole number");
             int number2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please choose any decimal number you'd like to add to your first input");
+            Console.WriteLine("Please choose any decimal number you'd like");
             decimal number3 = Decimal.Parse(Console.ReadLine());
-            Console.WriteLine(number2 * number3);
+            for (decimal value = number3; value == number3; value += .1m)
+            {
+                Console.WriteLine("The total of both your inputs is: " + number2 * number3, value, Decimal.Round(value));
+                break;
+            } 
+
+
+
+            //Console.WriteLine("The total of both your inputs is: " + number2 * number3);
             Console.ReadLine();
+
+            //Integers multiply = new Integers();
+            //multiply.MathOperations(number2, (int)number3);
+            //Console.WriteLine("The total is: " + number2 * number3);
+            //Console.ReadLine();
+
+
+
+
+
 
 
 
