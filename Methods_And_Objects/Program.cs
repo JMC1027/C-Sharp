9b64a0d566a50094aac00ee0f7f8ef35836ce616
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-12-14 17:23:31 -0500

    submit

--- Methods_And_Objects/Program.cs
+++ Methods_And_Objects/Program.cs
@@ -10,7 +10,14 @@ namespace Methods_And_Objects
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Type in your First Name");
+            string Fname = Console.ReadLine();
+            Console.ReadLine();
+            Console.WriteLine("Type in your Last Name");
+            string Lname = Console.ReadLine();
+            Console.ReadLine();
+            Console.WriteLine("Name: " + Fname + " " + Lname);
+            Console.ReadLine();
         }
     }
 }
