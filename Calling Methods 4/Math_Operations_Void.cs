b6a4c5b691b337b8ab1346616402c75ec03433ec
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-12-13 21:01:58 -0500

    submit

---
+++ Calling Methods 4/Math_Operations_Void.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_4
+{
+    public class Math_Operations_Void
+    {
+        public void math(int num1, int num2)
+        {
+            int x = num1 * 5;
+            Console.WriteLine(num2);
+        }
+
+    
+    }
+}
---
+++ Calling Methods 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math_Operations_Void Math_Operations_Void = new Math_Operations_Void();// instantiate the class 
+            Console.WriteLine("Pass in a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pass in a 2nd number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Math_Operations_Void.math(num1, num2);
+            Console.ReadLine();
+        }
+    }
+}
