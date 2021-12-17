2ae49a16e50c528b3841a1afa409eae076ec3b45
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-12-17 12:45:49 -0500

    push

---
+++ Time/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            Console.WriteLine(time.ToString("MM/dd/yyyy"));
+            Console.ReadLine();
+            Console.WriteLine("Input a number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+            TimeSpan z = new System.TimeSpan(x);
+            DateTime newTime = time.Add(x);
+            
+            //take user input and multiply it by current time 
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            //DateTime userInput = Convert.ToDateTime(Console.ReadLine());
+            //TimeSpan sum = x - userInput;
+            ////Console.WriteLine("");
+            //Console.ReadLine();
+            //take user input and display the time it ill be in that amount of hours
+            
+        }
+    }
+}
