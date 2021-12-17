adbc0e5a68e4e7c61ebb5d08fd3c466501bb6e7d
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-12-17 12:55:45 -0500

    submit

--- Time/Program.cs
+++ Time/Program.cs
@@ -11,14 +11,14 @@ namespace Time
         static void Main(string[] args)
         {
             DateTime time = DateTime.Now;
-            Console.WriteLine(time.ToString("MM/dd/yyyy"));
+            Console.WriteLine(time.ToString("MM/dd/yyyy"));//Prints current date to console
             Console.ReadLine();
-            Console.WriteLine("Input a number");
-            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input a number");//Asking user for an int input
+            int x = Convert.ToInt32(Console.ReadLine()); //user input = x Converts string to int
             Console.ReadLine();
-            TimeSpan z = new System.TimeSpan(x);
-            DateTime y = time.AddHours(x);
-            Console.WriteLine(y);
+            TimeSpan z = new System.TimeSpan(x);  //timespan z instantiates new timespan, using user input as object(x)
+            DateTime y = time.AddHours(x); //adding current date date to user input 
+            Console.WriteLine(y);//displays var y as conversion of user input and current date time
             Console.ReadLine();
            
         }
