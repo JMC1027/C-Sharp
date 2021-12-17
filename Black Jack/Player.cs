80cc5040b1c149823ec4db54d5a91f5332617331
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-12-16 19:15:28 -0500

    submit

--- Black Jack/Player.cs
+++ Black Jack/Player.cs
@@ -8,16 +8,37 @@ namespace Black_Jack
 {
     public class Player
     {
+        public Player(string name, int beginingBalance) //Constructor that takes 2 parameters
+        {
+            Hand = new List<Card>();
+            Balance = beginingBalance;
+            Name = name;
+        }
         public List<Card> Hand { get; set; }
         public int Balance { get; set; }
         public string Name { get; set; }
         public bool isActivelyPlaying { get; set; }
-        public static Game operator+(Game game, Player player)
+        public bool Stay { get; set; }
+
+        public bool Bet(int amount)
+        {
+            if (Balance - amount < 0)
+            {
+                Console.WriteLine("You do not have enough to place a bet that size.");
+                return false;
+            }
+            else
+            {
+                Balance -= amount;
+                return true;
+            }
+        }
+        public static Game operator +(Game game, Player player)
         {
             game.Players.Add(player);
             return game;
         }
-        public static Game operator-(Game game, Player player)
+        public static Game operator -(Game game, Player player)
         {
             game.Players.Remove(player);
             return game;
