ff6e678747ffa5b55f8359fac94fa5a30fee7204
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2022-01-01 13:06:58 -0500

    push

---
+++ Time/Pages/Privacy.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Time.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            DateTime serverTime = DateTime.Now;
+            DateTime utcTime = serverTime.ToUniversalTime();
+            ViewData["TimeStamp"] = DateTime.Now;
+        }
+    }
+}
--- Time/Program.cs
+++ Time/Program.cs
@@ -1,26 +1,26 @@
-﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Time
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            DateTime time = DateTime.Now;
-            Console.WriteLine(time.ToString("MM/dd/yyyy"));//Prints current date to console
-            Console.ReadLine();
-            Console.WriteLine("Input a number");//Asking user for an int input
-            int x = Convert.ToInt32(Console.ReadLine()); //user input = x Converts string to int
-            Console.ReadLine();
-            TimeSpan z = new System.TimeSpan(x);  //timespan z instantiates new timespan, using user input as object(x)
-            DateTime y = time.AddHours(x); //adding current date date to user input 
-            Console.WriteLine(y);//displays var y as conversion of user input and current date time
-            Console.ReadLine();
-           
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
